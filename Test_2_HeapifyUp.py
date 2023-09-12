d23abcb4db9cde5f6c70f50e905eda640eee97c0
Harlan Howe <hhowe7@gatech.edu>
2023-09-12 14:11:03 -0500

    Cleanup in Test2 for style

--- Test_2_HeapifyUp.py
+++ Test_2_HeapifyUp.py
@@ -1,16 +1,16 @@
 import unittest
 from PriorityQueueFile import PriorityQueue
 
+
 class MyTestCase(unittest.TestCase):
 
     def test_heap_add_1(self):
         # creating a min heap
         pq = PriorityQueue(isMinHeap=True)
         items = [[0, "AB"], [7, "CD"], [3, "EF"], [4, "GH"], [8, "IJ"], [10, "KL"], [5, "MN"], [13, "OP"], [6, "QR"],
-                 [12, "ST"], \
-                 [2, "UV"], [1, "WX"], [9, "YZ"]]
+                 [12, "ST"], [2, "UV"], [1, "WX"], [9, "YZ"]]
 
-        min_heap_tree = [[0, 'AB'], [2, 'UV'], [1, 'WX'], [6, 'QR'], [4, 'GH'], [3, 'EF'], [5, 'MN'], [13, 'OP'], \
+        min_heap_tree = [[0, 'AB'], [2, 'UV'], [1, 'WX'], [6, 'QR'], [4, 'GH'], [3, 'EF'], [5, 'MN'], [13, 'OP'],
                          [7, 'CD'], [12, 'ST'], [8, 'IJ'], [10, 'KL'], [9, 'YZ']]
 
         for item in items:
@@ -22,10 +22,10 @@ class MyTestCase(unittest.TestCase):
         # creating a max heap
         pq = PriorityQueue(isMinHeap=False)
         items = [[0, "AB"], [7, "CD"], [3, "EF"], [4, "GH"], [8, "IJ"], [10, "KL"], [5, "MN"], [13, "OP"], [6, "QR"],
-                 [12, "ST"], \
+                 [12, "ST"],
                  [2, "UV"], [1, "WX"], [9, "YZ"]]
 
-        max_heap_tree = [[13, 'OP'], [12, 'ST'], [9, 'YZ'], [7, 'CD'], [10, 'KL'], [8, 'IJ'], [5, 'MN'], [0, 'AB'], \
+        max_heap_tree = [[13, 'OP'], [12, 'ST'], [9, 'YZ'], [7, 'CD'], [10, 'KL'], [8, 'IJ'], [5, 'MN'], [0, 'AB'],
                          [6, 'QR'], [4, 'GH'], [2, 'UV'], [1, 'WX'], [3, 'EF']]
 
         for item in items:
@@ -45,9 +45,9 @@ class MyTestCase(unittest.TestCase):
             pq.add_value(value=item[1], priority=item[0])
 
         self.assertTrue(pq.is_a_heap(), "The tree should have become a heap.")
-        self.assertNotEqual(distractor,pq.my_tree, "Tree with two equal priorities did create heap, but tie should not have pushed original down.")
-        self.assertEqual(expected,pq.my_tree, "Tree results are incorrect.")
-
+        self.assertNotEqual(distractor, pq.my_tree, "Tree with two equal priorities did create heap, but tie "
+                                                    "should not have pushed original down.")
+        self.assertEqual(expected, pq.my_tree, "Tree results are incorrect.")
 
 
 if __name__ == '__main__':
