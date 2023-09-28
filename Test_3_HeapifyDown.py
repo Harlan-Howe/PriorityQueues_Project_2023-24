f03b2442a21638a62cb3725814d1cbaebe6f2e97
Harlan Howe <hhowe7@gatech.edu>
2023-09-28 12:02:49 -0500

    several more list[list] --> list[tuple] to make tests work.

--- Test_3_HeapifyDown.py
+++ Test_3_HeapifyDown.py
@@ -8,7 +8,7 @@ class MyTestCase(unittest.TestCase):
         checks whether a single-step heapify down works.
         :return:
         """
-        items = [[0, 1], [1, 2], [4, 5], [6, 11], [7, 13], [8, 17], [5, 7], [9, 19], [2, 3]]
+        items = [(0, 1), (1, 2), (4, 5), (6, 11), (7, 13), (8, 17), (5, 7), (9, 19), (2, 3)]
         expected = [(0, 1), (1, 2), (4, 5), (2, 3), (7, 13), (8, 17), (5, 7), (9, 19), (6, 11)]
 
         pq: PriorityQueue[int] = PriorityQueue[int](tree=items, is_min_heap=True)
@@ -22,8 +22,8 @@ class MyTestCase(unittest.TestCase):
         tests whether a choice between two equal priority children is handled correctly.
         :return:
         """
-        items = [[2, 'Happy'], [11, 'Dopey'], [5, 'Sneezy'], [7, 'Grumpy'], [7, 'Sleepy'], [8, 'Doc'], [6, 'Bashful']]
-        distractor = [[2, 'Happy'], [7, 'Sleepy'], [5, 'Sneezy'], [7, 'Grumpy'], [11, 'Dopey'], [8, 'Doc'],
+        items = [(2, 'Happy'), (11, 'Dopey'), (5, 'Sneezy'), (7, 'Grumpy'), (7, 'Sleepy'), (8, 'Doc'), (6, 'Bashful')]
+        distractor = [(2, 'Happy'), (7, 'Sleepy'), (5, 'Sneezy'), (7, 'Grumpy'), (11, 'Dopey'), (8, 'Doc'),
                       [6, 'Bashful']]
         expected = [(2, 'Happy'), (7, 'Grumpy'), (5, 'Sneezy'), (11, 'Dopey'), (7, 'Sleepy'), (8, 'Doc'),
                     (6, 'Bashful')]
@@ -76,13 +76,13 @@ class MyTestCase(unittest.TestCase):
         checks whether a heapify down from the root for a multistage tree works.
         :return:
         """
-        items = [[26, 'TX'], [47, 'NM'], [50, 'HI'], [38, 'CO'], [46, 'OK'], [49, 'AK'], [30, 'WI'], [33, 'OR'],
-                 [37, 'NB'], [41, 'MT'], [45, 'UT'], [48, 'AZ'], [24, 'MI'], [26, 'MI'], [29, 'IA'], [17, 'OH'],
-                 [32, 'MN'], [34, 'KS'], [36, 'NV'], [21, 'IL'], [40, 'SD'], [42, 'WA'], [44, 'WY'], [25, 'AR'],
-                 [31, 'CA'], [6, 'MA'], [23, 'ME'], [5, 'CT'], [13, 'RI'], [12, 'NC'], [27, 'FL'], [1, 'DE'],
-                 [10, 'VA'], [7, 'MD'], [22, 'AL'], [4, 'GA'], [16, 'TN'], [15, 'KY'], [35, 'WV'], [3, 'NJ'],
-                 [18, 'LA'], [9, 'NH'], [39, 'ND'], [8, 'SC'], [20, 'MS'], [19, 'IN'], [43, 'ID'], [2, 'PA'],
-                 [14, 'VT'], [11, 'NY']]
+        items = [(26, 'TX'), (47, 'NM'), (50, 'HI'), (38, 'CO'), (46, 'OK'), (49, 'AK'), (30, 'WI'), (33, 'OR'),
+                 (37, 'NB'), (41, 'MT'), (45, 'UT'), (48, 'AZ'), (24, 'MI'), (26, 'MI'), (29, 'IA'), (17, 'OH'),
+                 (32, 'MN'), (34, 'KS'), (36, 'NV'), (21, 'IL'), (40, 'SD'), (42, 'WA'), (44, 'WY'), (25, 'AR'),
+                 (31, 'CA'), (6, 'MA'), (23, 'ME'), (5, 'CT'), (13, 'RI'), (12, 'NC'), (27, 'FL'), (1, 'DE'),
+                 (10, 'VA'), (7, 'MD'), (22, 'AL'), (4, 'GA'), (16, 'TN'), (15, 'KY'), (35, 'WV'), (3, 'NJ'),
+                 (18, 'LA'), (9, 'NH'), (39, 'ND'), (8, 'SC'), (20, 'MS'), (19, 'IN'), (43, 'ID'), (2, 'PA'),
+                 (14, 'VT'), (11, 'NY')]
 
         expected = [(50, 'HI'), (47, 'NM'), (49, 'AK'), (38, 'CO'), (46, 'OK'), (48, 'AZ'), (30, 'WI'), (33, 'OR'),
                     (37, 'NB'), (41, 'MT'), (45, 'UT'), (31, 'CA'), (24, 'MI'), (26, 'MI'), (29, 'IA'), (17, 'OH'),
@@ -102,8 +102,8 @@ class MyTestCase(unittest.TestCase):
 
     def test_heapify_Down_6(self):
         pq: PriorityQueue[str] = PriorityQueue[str](is_min_heap=True)
-        items = [[0, "AB"], [7, "CD"], [3, "EF"], [4, "GH"], [8, "IJ"], [10, "KL"], [5, "MN"], [13, "OP"], [6, "QR"],
-                 [12, "ST"], [2, "UV"], [1, "WX"], [9, "YZ"]]
+        items = [(0, "AB"), (7, "CD"), (3, "EF"), (4, "GH"), (8, "IJ"), (10, "KL"), (5, "MN"), (13, "OP"), (6, "QR"),
+                 (12, "ST"), (2, "UV"), (1, "WX"), (9, "YZ")]
         items_after_pop = [(1, 'WX'), (2, 'UV'), (3, 'EF'), (6, 'QR'), (4, 'GH'), (9, 'YZ'), (5, 'MN'), (13, 'OP'),
                            (7, 'CD'), (12, 'ST'), (8, 'IJ'), (10, 'KL')]
         items_after_pop2 = [(2, 'UV'), (4, 'GH'), (3, 'EF'), (6, 'QR'), (8, 'IJ'), (9, 'YZ'), (5, 'MN'), (13, 'OP'),
@@ -115,9 +115,9 @@ class MyTestCase(unittest.TestCase):
             pq.add_value(value=item[1], priority=item[0])
 
         sorted_items = []
-        self.assertEqual([0, "AB"], pq.pop(), "Failed to pop smallest priority item from minHeap.")
+        self.assertEqual((0, "AB"), pq.pop(), "Failed to pop smallest priority item from minHeap.")
         self.assertEqual(items_after_pop, pq.my_tree, "myTree is no longer a min heap.")
-        self.assertEqual([1, "WX"], pq.pop(), "Failed to pop second smallest priority item from minHeap.")
+        self.assertEqual((1, "WX"), pq.pop(), "Failed to pop second smallest priority item from minHeap.")
         self.assertEqual(items_after_pop2, pq.my_tree, "myTree is no longer a min heap after second pop.")
         while not pq.is_empty():
             sorted_items.append(pq.pop())
@@ -132,8 +132,8 @@ class MyTestCase(unittest.TestCase):
         the right....
         :return:
         """
-        items = [[6, 'Jrnl'], [2, 'Dance'], [1, 'Choir'], [9, 'Theat'], [5, 'Photo'], [7, 'Film'], [3, 'Ceram'],
-                 [13, 'Orch'], [11, 'ChTh'], [14, 'MxMd'], [8, 'DrwPt'], [10, 'Tech'], [12, 'Sclp'], [4, 'Band']]
+        items = [(6, 'Jrnl'), (2, 'Dance'), (1, 'Choir'), (9, 'Theat'), (5, 'Photo'), (7, 'Film'), (3, 'Ceram'),
+                 (13, 'Orch'), (11, 'ChTh'), (14, 'MxMd'), (8, 'DrwPt'), (10, 'Tech'), (12, 'Sclp'), (4, 'Band')]
 
         expected = [(1, 'Choir'), (2, 'Dance'), (3, 'Ceram'), (9, 'Theat'), (5, 'Photo'), (7, 'Film'), (4, 'Band'),
                     (13, 'Orch'), (11, 'ChTh'), (14, 'MxMd'), (8, 'DrwPt'), (10, 'Tech'), (12, 'Sclp'), (6, 'Jrnl')]
